1e340086740a66a9594509ecb35c2c8ab329c637
TOJI <roshanmaurya1204@gmail.com>
2026-03-18 10:41:01 +0530

    Add files via upload

---
+++ MethodOVERloading.java
@@ -0,0 +1,20 @@
+// Method overloading = multiple methods with same name but different inputs
+
+//Same name + different parameters = Overloading
+// METHOD OVERLOADING CONCEPTS
+class cal
+{
+    public int add (int n1,int n2)
+    {
+        return n1+n2;
+    }
+    public int add (int n1,int n2,int n3)
+    {
+        return n1+n2;
+    }
+    public double add (double n1,double n2)
+    {
+        return n1+n2;
+    }
+}
+
